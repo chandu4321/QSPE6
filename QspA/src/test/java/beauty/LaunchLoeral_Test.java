d6bc43d460b2632b9cc2dd343b9d499b48d0a7f7
Admin <Admin@DESKTOP-8PDR25K>
2024-03-29 19:28:16 +0530

    I have added a TestScript(LaunchLoeral_Test) inside Beauty package.

---
+++ QspA/src/test/java/beauty/LaunchLoeral_Test.java
@@ -0,0 +1,12 @@
+package beauty;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LaunchLoeral_Test {
+
+	@Test
+	public void loeral() {
+		Reporter.log("test case",true);
+	}
+}
